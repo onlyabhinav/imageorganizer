7a6d465cf21075db45c4bff82dad97b7f26ce335
abhinav <abhinav.2310@gmail.com>
2021-01-03 23:35:41 +0530

    Adding code to GIT

---
+++ src/main/java/dev/abhinav/imageorganizer/ImageorganizerApplication.java
@@ -0,0 +1,16 @@
+package dev.abhinav.imageorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageorganizerApplication.class, args);
+
+
+		System.out.println("Helllo............");
+	}
+
+}
---
+++ src/test/java/dev/abhinav/imageorganizer/ImageorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abhinav.imageorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
